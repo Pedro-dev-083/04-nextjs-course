48199a712b8e4560b62916f3e2d6c8f2c4019503
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2023-12-21 19:04:50 -0300

    Aula 4

--- src/screens/FAQQuestionScreen/index.js
+++ src/screens/FAQQuestionScreen/index.js
@@ -2,6 +2,12 @@ import Head from 'next/head';
 import { Footer } from '../../components/commons/Footer';
 import { Menu } from '../../components/commons/Menu';
 import { Box, Text, theme } from '../../theme/components';
+import { cmsService } from '../../infra/cms/cmsService';
+import { StructuredText } from 'react-datocms/structured-text';
+import { renderNodeRule } from 'react-datocms'
+import { isHeading } from 'datocms-structured-text-utils';
+import CMSProvider from '../../infra/cms/CMSProvider';
+import { pageHOC } from '../../components/wrappers/pageHOC';
 
 export async function getStaticPaths() {
   return {
@@ -13,26 +19,35 @@ export async function getStaticPaths() {
   };
 }
 
-export function getStaticProps({ params }) {
+export async function getStaticProps({ params, preview }) {
   const { id } = params;
+
+  const contentQuery = `
+    query{
+      contentFaqQuestion{
+        title
+        content{
+          value      
+        }
+      }
+    }
+  `;
+  const {data} = await cmsService({
+    query: contentQuery,
+    preview: preview
+  })
+
   return {
     props: {
+      cmsContent: data,
       id,
-      title: 'Fake Title',
-      content: `
-        <h2>Primeiro Tópico</h2>
-        <p>paragrafo simples</p>
-        <p>outro paragrafo simples</p>
-        <ul>
-          <li>Item de lista 01</li>
-          <li>Item de lista 02</li>
-        </ul>
-      `,
+      title: data.contentFaqQuestion.title,
+      content: data.contentFaqQuestion.content,
     }
   }
 }
 
-export default function FAQQuestionScreen({ title, content }) {
+function FAQQuestionScreen({ cmsContent }) {
   return (
     <>
       <Head>
@@ -52,8 +67,6 @@ export default function FAQQuestionScreen({ title, content }) {
       >
         <Box
           styleSheet={{
-            display: 'flex',
-            gap: theme.space.x4,
             flexDirection: 'column',
             width: '100%',
             maxWidth: theme.space.xcontainer_lg,
@@ -61,14 +74,30 @@ export default function FAQQuestionScreen({ title, content }) {
           }}
         >
           <Text tag="h1" variant="heading1">
-            {title}
+            {cmsContent.contentFaqQuestion.title}
           </Text>
 
-          <Box dangerouslySetInnerHTML={{ __html: content }} />
+          {/* <Box dangerouslySetInnerHTML={{ __html: content }} /> */}
+          <StructuredText 
+            data={cmsContent.contentFaqQuestion.content}
+            customNodeRules={[
+              renderNodeRule(isHeading, ({node, children, key})=>{
+                const tag = `h${node.level}`;
+                const variant = `heading${node.level}`
+                return(
+                  <Text tag={tag} variant={variant} key={key}>
+                    {children}
+                  </Text>
+                )
+              })
+            ]}
+            />
         </Box>
       </Box>
 
-      <Footer />
+      <Footer/>
     </>
   )
 }
+
+export default pageHOC(FAQQuestionScreen);
